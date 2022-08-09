7585aa4dac9d8ea602f6ee151f6cb63e6dc02290
Winny <winny676@gmail.com>
2022-08-09 19:02:17 -0300

    Criação da Class Autor

---
+++ src/main/java/br/com/winny/modelo/Autor.java
@@ -0,0 +1,44 @@
+package br.com.winny.modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	@Column(name = "data_nascimento")
+	private LocalDate dataDeNascimento;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+}
